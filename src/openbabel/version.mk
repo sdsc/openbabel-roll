a4af48fc6628ea46a7c07f94ca06a83734c2e496
Jim Hayes <jhayes@sdsc.edu>
2018-02-27 13:42:21 -0800

    Set rpm make vars needed for Rocks v7. Resolves APS-696.

--- src/openbabel/version.mk
+++ src/openbabel/version.mk
@@ -1,6 +1,6 @@
 NAME           = sdsc-openbabel
 VERSION        = 2.3.2
-RELEASE        = 3
+RELEASE        = 4
 PKGROOT        = /opt/openbabel
 
 SRC_SUBDIR     = openbabel
@@ -11,6 +11,7 @@ SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_GZ_PKGS       = $(SOURCE_PKG)
+TAR_GZ_PKGS    = $(SOURCE_PKG)
 
-RPM.EXTRAS     = AutoReq:No
+RPM.EXTRAS     = AutoReq:No\nAutoProv:No
+RPM.PREFIX     = $(PKGROOT)
--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,7 @@
 NAME       = sdsc-openbabel-roll-test
 VERSION    = 1
-RELEASE    = 0
+RELEASE    = 2
 PKGROOT    = /root/rolltests
 
-RPM.EXTRAS = AutoReq:No
+RPM.EXTRAS = AutoReq:No\nAutoProv:No
+RPM.FILES = $(PKGROOT)/openbabel.t
